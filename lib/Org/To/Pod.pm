2a9fa635af17dd0eff2342c987a74fa2e98b29fb
Steven Haryanto <stevenharyanto@gmail.com>
2011-09-07 02:42:32 +0000

    Import of SHARYANTO/Org-To-Pod-0.00 from CPAN.
    gitpan-cpan-distribution: Org-To-Pod
    gitpan-cpan-version:      0.00
    gitpan-cpan-path:         SHARYANTO/Org-To-Pod-0.00.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Org/To/Pod.pm
@@ -0,0 +1,52 @@
+package Org::To::Pod;
+
+use 5.010;
+use Log::Any '$log';
+
+use vars qw($VERSION);
+
+our $VERSION = '0.00'; # VERSION
+
+1;
+# ABSTRACT: Export Org document to Pod
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Org::To::Pod - Export Org document to Pod
+
+=head1 VERSION
+
+version 0.00
+
+=head1 SYNOPSIS
+
+ use Org::To::Pod qw(org_to_pod);
+ my $pod = org_to_pod(source=>$org);
+
+=head1 DESCRIPTION
+
+NOT YET IMPLEMENTED.
+
+=head1 SEE ALSO
+
+L<Org::Parser>
+
+L<Org::To::HTML>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
